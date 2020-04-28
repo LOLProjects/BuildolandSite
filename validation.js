ecbcd84ab892ece340e11082ea9dc0fd37336b2a
amrojjeh <amrojjeh@gmail.com>
2020-04-28 16:51:46 -0500

    Fixed js; Changed profile

--- validation.js
+++ validation.js
@@ -2,6 +2,18 @@ function validateUsername(input)
 {
 	if (input.value.includes(" "))
 		input.setCustomValidity("Spaces aren't allowed")
-	else
+	else if (!input.value.match("[_0-9A-Za-z-]*"))
 		input.setCustomValidity("Only numbers, letters, underscores, and dashes are allowed")
+	else
+		input.setCustomValidity("")
+}
+
+function passChanged(input)
+{
+	let field;
+	if (input.id == "newp")
+		field = document.getElementById("oldp");
+	else
+		field = document.getElementById("newp");
+	field.required = (input.value != "");
 }
