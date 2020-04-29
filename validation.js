c9e4290a3d31bebdd015d0c963effcc2be77ebcd
amrojjeh <amrojjeh@gmail.com>
2020-04-29 03:36:30 -0500

    Updated regex

--- validation.js
+++ validation.js
@@ -2,7 +2,7 @@ function validateUsername(input)
 {
 	if (input.value.includes(" "))
 		input.setCustomValidity("Spaces aren't allowed")
-	else if (!input.value.match("[_0-9A-Za-z-]*"))
+	else if (!input.value.match("^[_0-9A-Za-z-]+$"))
 		input.setCustomValidity("Only numbers, letters, underscores, and dashes are allowed")
 	else
 		input.setCustomValidity("")
